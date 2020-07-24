30a78872f6c89afe5743bfc45b4ae7371f78e18e
NVeena30919 <nerendlaveena@gmail.com>
2020-07-24 13:03:26 +0530

    JUnit Task

---
+++ JUnitTask/src/com/epam/junitTask/Main.java
@@ -0,0 +1,19 @@
+package com.epam.junitTask;
+
+public class Main 
+{
+    String removeA(String s)
+	{
+		String res = "";
+		for(int i=0;i<s.length();i++)
+		{
+			if((i==0 || i==1) && s.charAt(i)=='A')
+				continue;
+			else
+				res+=s.charAt(i);	
+		}
+		return res;
+		
+	}
+
+}
